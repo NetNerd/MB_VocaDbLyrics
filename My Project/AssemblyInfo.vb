b15d3d27bc7bb3aa39bb51dad032da010dab046f
netnerd <justin.nn.dlr@gmail.com>
2015-06-06 23:43:52 +1000

    Assembly versions are now correct to what I'm calling the version (0.1) and the generated .zip is now named with the version and build config type.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("f72cb066-8590-48c1-b531-9eafe3b72770")> 
+<Assembly: Guid("f72cb066-8590-48c1-b531-9eafe3b72770")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("0.1")>
+<Assembly: AssemblyFileVersion("0.1")>
